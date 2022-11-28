1fb2467155a55856fa31a2403d0e8ecad80f1ef2
ganapatiKabbargi <ganapatisk96@gmail.com>
2022-11-28 22:02:38 +0530

    making component smaller and readable

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,18 +1,15 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetais";
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
-  //   const date = new Date(2021, 2, 28);
-  //   const title = "car insurance";
-  //   const location = "insurance office";
-  //   const amount = 200;
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className="expense-item__price">$ {props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDetails
+        amount={props.amount}
+        title={props.title}
+      ></ExpenseDetails>
     </div>
   );
 }

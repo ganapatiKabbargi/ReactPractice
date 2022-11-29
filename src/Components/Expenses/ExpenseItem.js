2951a6eb4fac6b36bcf9e24aa29237b9d67b86e3
ganapatiKabbargi <ganapatisk96@gmail.com>
2022-11-29 10:51:32 +0530

    event listners added

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -3,6 +3,8 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
+  const clickHandler = () => {};
+  const deleteclickHandler = () => {};
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +12,12 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
+      <button className="expense-item__Ebutton" onClick={clickHandler}>
+        Change title
+      </button>
+      <button className="expense-item__Dbutton" onClick={deleteclickHandler}>
+        Delete Expense
+      </button>
     </Card>
   );
 };

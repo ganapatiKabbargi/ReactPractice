2db44cad2fe13d0a1031812f2710d395c82a75f3
ganapatiKabbargi <ganapatisk96@gmail.com>
2022-11-30 16:45:52 +0530

    conditional rendering

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -20,16 +20,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       ></ExpensesFilter>
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
---
+++ src/Components/Expenses/ExpensesList.js
@@ -0,0 +1,46 @@
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpenseList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback"> found no expenses...</h2>;
+  }
+
+  if (props.items.length === 1) {
+    return (
+      <ul className="expenses-list">
+        {props.items.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+        <h3 className="expenses-list__fallback">
+          Only single expense here.Please add more...
+        </h3>
+        ;
+      </ul>
+    );
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ExpenseList;
